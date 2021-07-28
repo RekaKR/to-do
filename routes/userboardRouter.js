d75e4f4be7ab057c31da2370f8e0807e47e24f86
RekaKR <RekaKR@users.noreply.github.com>
2021-07-28 15:09:23 +0200

    Update get

--- routes/userboardRouter.js
+++ routes/userboardRouter.js
@@ -2,32 +2,37 @@ const express = require('express')
 const router = express.Router()
 const UserBoard = require('../models/userBoardModel')
 
-//const UserBoard = require('../models/userBoardModel')
 
 router.get('/', async (req, res) => {
-  const userId = req.headers['x-user-id']
+  const user_id = req.headers['x-user-id']
 
-  if (!userId) return res.status(401).json({ message: "x-user-id missing" })
+  if (!user_id) return res.status(401).json({ message: "x-user-id missing" })
 
-  const userBoards = await UserBoard.findOne({ user_id: userId })
+  const userBoard = await UserBoard.findOneAndUpdate(
+    { user_id },
+    { user_id },
+    { upsert: true, new: true }
+  )
 
-  if (userBoards) return res.json(userBoards)
+  res.status(200).json(userBoard)
 
-  const userB = new UserBoard({
-    user_id: userId,
-    dashboards: []
-  })
-
-  //try {
-  const savedUserB = await userB.save()
-  res.status(200).json(savedUserB)
   /*
-  } catch (err) {
-    res.json({ message: "HIBA VAN A SAVE-NÉL" })
-  }
+    const userBoard = await UserBoard.findOne({ user_id: user_id })
+  
+    if (userBoard) return res.json(userBoard)
+  
+    const userB = new UserBoard({
+      user_id: user_id,
+      dashboards: []
+    })
+  
+    //try {
+    const savedUserB = await userB.save()
+    res.status(200).json(savedUserB)
+    //  } catch (err) {
+    //    res.json({ message: "HIBA VAN A SAVE-NÉL" })
+    //  }
   */
-
-  //  res.status(200).json(userBoards)
 })
 
 
